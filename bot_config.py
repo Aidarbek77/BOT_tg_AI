39a1b6d6f3e71979ea253d66712bb32c06c07381
AIdarbek47 <151944142+Aidarbek77@users.noreply.github.com>
2024-10-22 17:41:42 +0600

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -5,8 +5,6 @@ from database.database import Database
 
 
 token = dotenv_values('.env')['BOT_TOKEN']
-if not token:
-    token = "7270594509:AAG8ovcPAwujaBMdIKoAqwr5tIg98LELOdw"
 bot = Bot(token=token)
 dp = Dispatcher()
-database = Database("db.sqlite")
+database = Database("db.sqlite3")
