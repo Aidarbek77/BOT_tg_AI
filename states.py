18a4263c36437ce581632c3c7d4b69c78f6612bb
AIdarbek47 <151944142+Aidarbek77@users.noreply.github.com>
2024-10-31 21:50:49 +0600

    Add files via upload

---
+++ states.py
@@ -0,0 +1,9 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+class RestaurantReview(StatesGroup):
+    name = State()
+    phone_or_instagram = State()
+    visit_date = State()
+    food_rating = State()
+    cleanliness_rating = State()
+    extra_comments = State()
