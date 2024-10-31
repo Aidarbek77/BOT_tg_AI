5a413cededac4d61c741c0ec3212cb346adacc8b
AIdarbek47 <151944142+Aidarbek77@users.noreply.github.com>
2024-10-31 21:49:03 +0600

    Delete handlers directory

--- handlers/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-class RestaurantReview(StatesGroup):
-    name = State()
-    phone_or_instagram = State()
-    visit_date = State()
-    food_rating = State()
-    cleanliness_rating = State()
-    extra_comments = State()
