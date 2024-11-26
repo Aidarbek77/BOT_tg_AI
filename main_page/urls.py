30cd10bdfca0c7a395bf1310c1fbeb81a378d8b3
AIdarbek47 <151944142+Aidarbek77@users.noreply.github.com>
2024-11-26 17:48:15 +0600

    Delete main_page directory

--- main_page/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.Bo.as_view(), name='books_list'),
-    path('book_detail/<int:id>/', views.BookListView.as_view(), name='detail'),
-    path('search/', views.SearchView.as_view(), name='search'),
-]
